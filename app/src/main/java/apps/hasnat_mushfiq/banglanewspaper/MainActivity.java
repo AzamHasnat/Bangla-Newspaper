29271ac68ea1d2a4b37529d819348a9b19f20ce9
AzamHasnat <hasnat.azam@g.bracu.ac.bd>
2021-04-23 18:00:35 +0600

    Add webview in Project

---
+++ app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/MainActivity.java
@@ -0,0 +1,54 @@
+package apps.hasnat_mushfiq.banglanewspaper;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.annotation.SuppressLint;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    WebView webView;
+    String url = "";
+    String names = "";
+    TextView name;
+
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Window window = MainActivity.this.getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(ContextCompat.getColor(MainActivity.this, R.color.black));
+
+        url = getIntent().getStringExtra("url");
+        names = getIntent().getStringExtra("url");
+
+        webView = findViewById(R.id.webview);
+        name = findViewById(R.id.name);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl(url);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+
+    @Override
+    public void onBackPressed() {
+
+        if (webView.canGoBack()){
+            webView.goBack();
+        }else super.onBackPressed();
+    }
+}
