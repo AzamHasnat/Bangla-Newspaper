72463fd6bbab76ec62829c4a436e5cc95c0b0b64
AzamHasnat <hasnat.azam@g.bracu.ac.bd>
2021-05-10 17:53:50 +0600

    Add on clicks on bangla newspaper

--- app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/Bangla.java
+++ app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/Bangla.java
@@ -3,6 +3,7 @@ package apps.hasnat_mushfiq.banglanewspaper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Bangla extends AppCompatActivity {
 
@@ -10,5 +11,95 @@ public class Bangla extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bangla);
+    }
+
+    public void prothomalo(View view) {
+    }
+
+    public void kalerkontho_bangla(View view) {
+    }
+
+    public void ittefaq_bangla(View view) {
+    }
+
+    public void noyadigonto_bangla(View view) {
+    }
+
+    public void bdprotidin_bangla(View view) {
+    }
+
+    public void jugantor_bangla(View view) {
+    }
+
+    public void amarshongbad_bangla(View view) {
+    }
+
+    public void protidiner_shongbad_bangla(View view) {
+    }
+
+    public void shongram_bangla(View view) {
+    }
+
+    public void manob_jomin_bangla(View view) {
+    }
+
+    public void amader_somoy_bangla(View view) {
+    }
+
+    public void samakal_bangla(View view) {
+    }
+
+    public void janakantha_bangla(View view) {
+    }
+
+    public void jayjaydin_bangla(View view) {
+    }
+
+    public void vorerkagaj_bangla(View view) {
+    }
+
+    public void ARTHONITEER_KAGOJ(View view) {
+    }
+
+    public void INQILAB(View view) {
+    }
+
+    public void SANGBAD(View view) {
+    }
+
+    public void MANOB_KANTHA(View view) {
+    }
+
+    public void SUPROVAT(View view) {
+    }
+
+    public void BONKI_BARTA(View view) {
+    }
+
+    public void Daily_DINKAL(View view) {
+    }
+
+    public void ALOKITO_BANGLADESH(View view) {
+    }
+
+    public void AJKER_BAZAR(View view) {
+    }
+
+    public void AMADER_AURTHONEETI(View view) {
+    }
+
+    public void BANGLADESH_POST(View view) {
+    }
+
+    public void SOREJOMIN_BARTA(View view) {
+    }
+
+    public void AJKER_PATRIKA(View view) {
+    }
+
+    public void KHABAR_PATRA(View view) {
+    }
+
+    public void VORER_PATA(View view) {
     }
 }
