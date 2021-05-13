6450365d55f7a5a8b2085f694ce18b633e154c7c
AzamHasnat <hasnat.azam@g.bracu.ac.bd>
2021-05-13 16:00:23 +0600

    update sports

--- app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/Sports.java
+++ app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/Sports.java
@@ -3,6 +3,7 @@ package apps.hasnat_mushfiq.banglanewspaper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Sports extends AppCompatActivity {
 
@@ -10,5 +11,29 @@ public class Sports extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sports);
+    }
+
+    public void SPORTS_CANVASS(View view) {
+    }
+
+    public void PAVILION(View view) {
+    }
+
+    public void BD_SPORTS_NEWS(View view) {
+    }
+
+    public void TIGER_CRICKET(View view) {
+    }
+
+    public void DAILY_SPORTS_BD(View view) {
+    }
+
+    public void BD_NEWS_SPORTS(View view) {
+    }
+
+    public void UNB(View view) {
+    }
+
+    public void JAGO_NEWS(View view) {
     }
 }
