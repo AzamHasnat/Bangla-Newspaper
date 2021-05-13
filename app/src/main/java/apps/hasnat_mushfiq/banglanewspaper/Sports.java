62beb6d81bd8fce6f6167f56d1a05f79e860536f
AzamHasnat <hasnat.azam@g.bracu.ac.bd>
2021-05-13 16:28:42 +0600

    update technology

--- app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/Sports.java
+++ app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/Sports.java
@@ -2,6 +2,7 @@ package apps.hasnat_mushfiq.banglanewspaper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,26 +15,58 @@ public class Sports extends AppCompatActivity {
     }
 
     public void SPORTS_CANVASS(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void PAVILION(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BD_SPORTS_NEWS(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void TIGER_CRICKET(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void DAILY_SPORTS_BD(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BD_NEWS_SPORTS(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void UNB(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void JAGO_NEWS(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 }
--- app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/Technology.java
+++ app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/Technology.java
@@ -2,7 +2,9 @@ package apps.hasnat_mushfiq.banglanewspaper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Technology extends AppCompatActivity {
 
@@ -10,5 +12,54 @@ public class Technology extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_technology);
+    }
+
+    public void BANGLA_TECH24(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void BIZ_TECH24(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void DAILY_ICT_NEWS(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void TECH_ZOOM(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void DIGIT(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void TECH_GUP(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void ASIANET_NEWS_TECH(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 }
