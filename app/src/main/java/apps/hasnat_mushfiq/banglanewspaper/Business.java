c4de1d1ce49d7d8dcedd055da258466e9a28f365
mushfiq122 <mushfiqur14-764@diu.edu.bd>
2021-05-13 16:41:08 +0600

    completed international and business news

--- app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/Business.java
+++ app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/Business.java
@@ -2,7 +2,9 @@ package apps.hasnat_mushfiq.banglanewspaper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Business extends AppCompatActivity {
 
@@ -10,5 +12,110 @@ public class Business extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_business);
+    }
+
+    public void ARThOSUCHAK(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","https://www.arthosuchak.com/");
+        intent.putExtra("name","Arthosuchak");
+        startActivity(intent);
+    }
+
+    public void CHATTAGRAM_STOCK_EXCHANGE(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","https://www.cse.com.bd/");
+        intent.putExtra("name","Chattagram Stock Exchange");
+        startActivity(intent);
+    }
+
+    public void Dhaka_stock_exchange(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","https://www.dsebd.org/");
+        intent.putExtra("name","Dhaka Stock Exchange");
+        startActivity(intent);
+    }
+
+    public void SHARE_BIZ(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","http://www.dailysharebiz.net/");
+        intent.putExtra("name","Share Biz");
+        startActivity(intent);
+    }
+
+    public void SHARE_MARKET_BD(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","https://www.sharemarketbd.com/");
+        intent.putExtra("name","Share Market BD");
+        startActivity(intent);
+    }
+
+    public void STOCK_BANGLADESH(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","https://stockbangladesh.com/");
+        intent.putExtra("name","Stock Bangladesh");
+        startActivity(intent);
+    }
+
+    public void THE_FINANCIAL_EXPRESS(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","https://thefinancialexpress.com.bd/");
+        intent.putExtra("name","The Financial Express");
+        startActivity(intent);
+    }
+
+    public void BUSINESS_NEWS_24_BD(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","https://businessnews24bd.com/");
+        intent.putExtra("name","Business News 24 BD");
+        startActivity(intent);
+    }
+
+    public void PUZI_BAZAR(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","http://pujibazar.com/");
+        intent.putExtra("name","Puzi Bazar");
+        startActivity(intent);
+    }
+
+    public void DAILY_STOCK_BANGLADESH(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","https://www.dailystockbangladesh.com/");
+        intent.putExtra("name","Daily Stock Bangladesh");
+        startActivity(intent);
+    }
+
+    public void SHARE_BUSINESS_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","http://www.sharebusiness24.com/");
+        intent.putExtra("name","Share Business 24");
+        startActivity(intent);
+    }
+
+    public void AJKER_BAZAR(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","https://www.ajkerbazzar.com/");
+        intent.putExtra("name","Ajker Bazar");
+        startActivity(intent);
+    }
+
+    public void BONIK_BARTA(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","https://bonikbarta.net/");
+        intent.putExtra("name","Bonik Barta");
+        startActivity(intent);
+    }
+
+    public void SHARE_NEWS_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","http://www.sharenews24.com/");
+        intent.putExtra("name","Share News 24");
+        startActivity(intent);
+    }
+
+    public void SHARE_BAZAR_NEWS(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","http://www.sharebazarnews.com/");
+        intent.putExtra("name","Share Bazar News");
+        startActivity(intent);
     }
 }
--- app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/International.java
+++ app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/International.java
@@ -2,7 +2,9 @@ package apps.hasnat_mushfiq.banglanewspaper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class International extends AppCompatActivity {
 
@@ -10,5 +12,76 @@ public class International extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_international);
+    }
+
+    public void THE_GUARDIAN(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","THE GUARDIAN (UK): https://www.theguardian.com/uk");
+        intent.putExtra("name","THE GUARDIAN (UK)");
+        startActivity(intent);
+    }
+
+    public void THE_WALL_STREET_JOURNAL(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","https://www.wsj.com/");
+        intent.putExtra("name","THE WALL STREET JOURNAL (USA)");
+        startActivity(intent);
+    }
+
+    public void THE_NEW_YORK_TIMES(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","https://www.nytimes.com/");
+        intent.putExtra("name","THE NEW YORK TIMES (USA)");
+        startActivity(intent);
+    }
+
+    public void THE_WASHINGTON_POST(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","https://www.washingtonpost.com/");
+        intent.putExtra("name","THE WASHINGTON POST (USA)");
+        startActivity(intent);
+    }
+
+    public void CHINA_DAILY(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","http://global.chinadaily.com.cn/");
+        intent.putExtra("name","CHINA DAILY (CHINA)");
+        startActivity(intent);
+    }
+
+    public void THE_TIMES_OF_INDIA(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","https://timesofindia.indiatimes.com/");
+        intent.putExtra("name","THE TIMES OF INDIA (INDIA)");
+        startActivity(intent);
+    }
+
+    public void THE_SYDNEY_MORNING_HERALD(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","https://www.smh.com.au/");
+        intent.putExtra("name","THE SYDNEY MORNING HERALD (AUSTRALIA)");
+        startActivity(intent);
+    }
+
+    public void ZAMAN(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","http://www.dailyzaman.com/ ");
+        intent.putExtra("name","ZAMAN (TURKEY)");
+        startActivity(intent);
+    }
+
+
+    public void DAWN(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","https://www.dawn.com/");
+        intent.putExtra("name","DAWN (PAKISTAN)");
+        startActivity(intent);
+    }
+
+    public void THE_ASAHI_SHIMBUN(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","http://www.asahi.com/ajw/");
+        intent.putExtra("name","THE ASAHI SHIMBUN (JAPAN)");
+        startActivity(intent);
     }
 }
