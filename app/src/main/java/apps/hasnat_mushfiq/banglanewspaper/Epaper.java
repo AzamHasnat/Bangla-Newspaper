ff62629ae2d884580b7840e9cbab137af154882f
AzamHasnat <hasnat.azam@g.bracu.ac.bd>
2021-05-13 15:47:51 +0600

    update e-paper

--- app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/Epaper.java
+++ app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/Epaper.java
@@ -2,7 +2,9 @@ package apps.hasnat_mushfiq.banglanewspaper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Epaper extends AppCompatActivity {
 
@@ -10,5 +12,117 @@ public class Epaper extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_epaper);
+    }
+
+    public void ALOKITO_BANGLADESH(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void BHORER_DAK(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void BHORER_KAGAJ(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void BANIK_BARTA(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void BANGLADESH_PROTIDIN(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void DAILY_JANAKANTHA(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void AMADER_SOMOY(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void DESH_RUPANTOR(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void JUGANTOR(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void KALER_KANTHO(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void PROTHOM_ALO(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void SAMAKAL(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void JAI_JAI_DIN(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void MANAB_KANTHO(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void PROTIDINER_SANGBAD(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
+    }
+
+    public void SOMOYER_ALO(View view) {
+    Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 }
