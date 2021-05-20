e1e3997a2b0c8de674af19deeb626d00805594b7
mushfiq122 <mushfiqur14-764@diu.edu.bd>
2021-05-20 01:09:25 -0700

    news portal

--- app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/NewsPortal.java
+++ app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/NewsPortal.java
@@ -3,6 +3,7 @@ package apps.hasnat_mushfiq.banglanewspaper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class NewsPortal extends AppCompatActivity {
 
@@ -10,5 +11,56 @@ public class NewsPortal extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_portal);
+    }
+
+    public void ONE_NEWS_BD(View view) {
+    }
+
+    public void AB_NEWS_24(View view) {
+    }
+
+    public void CHANGE_TV_PRESS(View view) {
+    }
+
+    public void AB_NEWS_24_BD(View view) {
+    }
+
+    public void ALOKITO_PROTIDIN(View view) {
+    }
+
+    public void AMADER_SOMOY_DOT_COM(View view) {
+    }
+
+    public void ASIAN_NEWS_OF_BANGLADESH(View view) {
+    }
+
+    public void APARAJEYO_BANGLA(View view) {
+    }
+
+    public void BANGLA_INSIDER(View view) {
+    }
+
+    public void BANGLA_NEWS_24(View view) {
+    }
+
+    public void BANGLA_TELEGRAPH(View view) {
+    }
+
+    public void BANGLA_TRIBUNE(View view) {
+    }
+
+    public void BANGLADESH_ONLINE_24(View view) {
+    }
+
+    public void BARTA_24(View view) {
+    }
+
+    public void BARTA_BANGLA(View view) {
+    }
+
+    public void BARTA_KONTHO(View view) {
+    }
+
+    public void BD_24_LIVE(View view) {
     }
 }
