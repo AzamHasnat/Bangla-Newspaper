a4452fd8d26b4c86bcdd8ba5ca96705e0e80d938
mushfiq122 <mushfiqur14-764@diu.edu.bd>
2021-05-22 20:48:17 -0700

    news portal java update

--- app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/NewsPortal.java
+++ app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/NewsPortal.java
@@ -2,6 +2,7 @@ package apps.hasnat_mushfiq.banglanewspaper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -14,155 +15,359 @@ public class NewsPortal extends AppCompatActivity {
     }
 
     public void ONE_NEWS_BD(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void AB_NEWS_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void CHANGE_TV_PRESS(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void AB_NEWS_24_BD(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void ALOKITO_PROTIDIN(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void AMADER_SOMOY_DOT_COM(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void ASIAN_NEWS_OF_BANGLADESH(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void APARAJEYO_BANGLA(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BANGLA_INSIDER(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BANGLA_NEWS_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BANGLA_TELEGRAPH(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BANGLA_TRIBUNE(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BANGLADESH_ONLINE_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BARTA_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BARTA_BANGLA(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BARTA_KONTHO(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BD_24_LIVE(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BD_TIMES_365(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BD_TODAY_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BD_TYPE(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BD_NEWS_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BREAKING_NEWS(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void BSS_NEWS(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void CHASHI_SHEBA(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void DAILY_BANGLADESH(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void DHAKA_TIMES_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void DMP_NEWS(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void FNS_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void FAST_NEWS_BD(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void GO_NEWS_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void HAWKER(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void DHAKA_NEWS_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void JAGO_NEWS_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void KACHER_DEYAL(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void LAKHO_KANTHO(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void LIVE_PRESS_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void NAGORIK_BARTA(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void NATUN_BARTA(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void NEWS_BANGLADESH(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void NIRAPAD_NEWS(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void NOTUN_KHOBOR(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void ODHIKAR_NEWS(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void PRIME_NEWS_BD(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void RISING_BD(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void RTNN(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void SAHOS_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void SAMAKAL_NEWS_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void SARA_BANGLA(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void SHERSHA_NEWS_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void SOMOYER_KONTHOSOR(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 
     public void THE_REPORT_24(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.putExtra("url","");
+        intent.putExtra("name","");
+        startActivity(intent);
     }
 }
