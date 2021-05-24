78ef24590233ad6791f4f69d7a5a734f3b14f959
mushfiq122 <mushfiqur14-764@diu.edu.bd>
2021-05-24 09:48:12 +0600

    completed sports

--- app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/Sports.java
+++ app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/Sports.java
@@ -16,57 +16,57 @@ public class Sports extends AppCompatActivity {
 
     public void SPORTS_CANVASS(View view) {
     Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","");
-        intent.putExtra("name","");
+        intent.putExtra("url","https://www.sportscanvass.com/");
+        intent.putExtra("name","SPORTS CANVASS");
         startActivity(intent);
     }
 
     public void PAVILION(View view) {
     Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","");
-        intent.putExtra("name","");
+        intent.putExtra("url","https://pavilion.com.bd/bn");
+        intent.putExtra("name","PAVILION");
         startActivity(intent);
     }
 
     public void BD_SPORTS_NEWS(View view) {
     Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","");
-        intent.putExtra("name","");
+        intent.putExtra("url","http://bdsportsnews.com/");
+        intent.putExtra("name","BD SPORTS NEWS");
         startActivity(intent);
     }
 
     public void TIGER_CRICKET(View view) {
     Intent intent = new Intent(this, MainActivity.class);
         intent.putExtra("url","");
-        intent.putExtra("name","");
+        intent.putExtra("name","TIGER CRICKET");
         startActivity(intent);
     }
 
     public void DAILY_SPORTS_BD(View view) {
     Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","");
-        intent.putExtra("name","");
+        intent.putExtra("url","https://sportsbangla.com/");
+        intent.putExtra("name","DAILY SPORTS BD");
         startActivity(intent);
     }
 
     public void BD_NEWS_SPORTS(View view) {
     Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","");
-        intent.putExtra("name","");
+        intent.putExtra("url","https://bdnews24.com/sport/");
+        intent.putExtra("name","BD NEWS SPORTS");
         startActivity(intent);
     }
 
     public void UNB(View view) {
     Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","");
-        intent.putExtra("name","");
+        intent.putExtra("url","https://unb.com.bd/category/17/Sports");
+        intent.putExtra("name","UNB");
         startActivity(intent);
     }
 
     public void JAGO_NEWS(View view) {
     Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","");
-        intent.putExtra("name","");
+        intent.putExtra("url","https://www.jagonews24.com/sports");
+        intent.putExtra("name","JAGO_NEWS");
         startActivity(intent);
     }
 }
