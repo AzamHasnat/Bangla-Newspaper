144d8d956a812f4065819debda837584cc33d0e7
mushfiq122 <mushfiqur14-764@diu.edu.bd>
2021-05-30 12:46:32 +0600

    added int bangla

--- app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/Int_Bangla.java
+++ app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/Int_Bangla.java
@@ -3,6 +3,7 @@ package apps.hasnat_mushfiq.banglanewspaper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class Int_Bangla extends AppCompatActivity {
 
@@ -10,5 +11,128 @@ public class Int_Bangla extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_int__bangla);
+    }
+
+    public void ukbdnews(View view) {
+    }
+
+    public void BengaliTimes(View view) {
+    }
+
+    public void Vao_Bangla(View view) {
+    }
+
+    public void bdprotidin_bangla(View view) {
+    }
+
+    public void China_Radio_Bangla(View view) {
+    }
+
+    public void Ananabazar_Patrika(View view) {
+    }
+
+    public void Zee_24_Ghonta(View view) {
+    }
+
+    public void Sangbad_Protidin(View view) {
+    }
+
+    public void Bartaman_Patrika(View view) {
+    }
+
+    public void kolkata24x7(View view) {
+    }
+
+    public void abpananda(View view) {
+    }
+
+    public void Sananda(View view) {
+    }
+
+    public void AAjkal_net(View view) {
+    }
+
+    public void ARTHONITEER_KAGOJ(View view) {
+    }
+
+    public void eisamay(View view) {
+    }
+
+    public void Ganashakti(View view) {
+    }
+
+    public void suprovat(View view) {
+    }
+
+    public void anandalok(View view) {
+    }
+
+    public void Asomiya_Pratidin(View view) {
+    }
+
+    public void dainiksambad(View view) {
+    }
+
+    public void amarshongbad_bangla(View view) {
+    }
+
+    public void UN_Bangla_Radio(View view) {
+    }
+
+    public void ALOKITO_BANGLADESH(View view) {
+    }
+
+    public void Bangla_Times(View view) {
+    }
+
+    public void Surma_News(View view) {
+    }
+
+    public void BANGLADESH_POST(View view) {
+    }
+
+    public void UK_Bengali(View view) {
+    }
+
+    public void Weekly_Bangladesh(View view) {
+    }
+
+    public void Deshebideshe(View view) {
+    }
+
+    public void deshebideshe(View view) {
+    }
+
+    public void KHABAR_PATRA(View view) {
+    }
+
+    public void gssnews(View view) {
+    }
+
+    public void londonbanglanews(View view) {
+    }
+
+    public void oneindia(View view) {
+    }
+
+    public void weeklybangalee(View view) {
+    }
+
+    public void thikana(View view) {
+    }
+
+    public void Parichoy(View view) {
+    }
+
+    public void parstoday(View view) {
+    }
+
+    public void Weekly_Janomot(View view) {
+    }
+
+    public void BBC_Bangla(View view) {
+    }
+
+    public void NHK_World(View view) {
     }
 }
