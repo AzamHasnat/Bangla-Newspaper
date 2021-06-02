98ad68db570925e713185d45546cab344b5ad059
mushfiq122 <mushfiqur14-764@diu.edu.bd>
2021-06-02 11:22:04 +0600

    completed tv news

--- app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/TvNews.java
+++ app/src/main/java/apps/hasnat_mushfiq/banglanewspaper/TvNews.java
@@ -56,100 +56,316 @@ public class TvNews extends AppCompatActivity {
     }
 
     public void CHANNEL_24(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","https://www.channel24bd.tv/");
-        intent.putExtra("name","Channel 24");
-        startActivity(intent);
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAGs", "The interstitial wasn't loaded yet.");
+        }
+
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                Intent intent = new Intent(TvNews.this, MainActivity.class);
+                intent.putExtra("url","https://www.channel24bd.tv/");
+                intent.putExtra("name","Channel 24");
+                startActivity(intent);
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
     }
 
     public void BANGLA_VISION_NEWS(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","https://www.bvnews24.com/");
-        intent.putExtra("name","Bangla Vision News");
-        startActivity(intent);
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAGs", "The interstitial wasn't loaded yet.");
+        }
+
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                Intent intent = new Intent(TvNews.this, MainActivity.class);
+                intent.putExtra("url","https://www.bvnews24.com/");
+                intent.putExtra("name","Bangla Vision News");
+                startActivity(intent);
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
     }
 
     public void CHANNEL_i(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","https://www.channelionline.com/");
-        intent.putExtra("name","Channei i");
-        startActivity(intent);
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAGs", "The interstitial wasn't loaded yet.");
+        }
+
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                Intent intent = new Intent(TvNews.this, MainActivity.class);
+                intent.putExtra("url","https://www.channelionline.com/");
+                intent.putExtra("name","Channei i");
+                startActivity(intent);
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
     }
 
     public void EKUSHE_TV(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","https://ekushey-tv.com/");
-        intent.putExtra("name","Ekushey Tv");
-        startActivity(intent);
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAGs", "The interstitial wasn't loaded yet.");
+        }
+
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                Intent intent = new Intent(TvNews.this, MainActivity.class);
+                intent.putExtra("url","https://ekushey-tv.com/");
+                intent.putExtra("name","Ekushey Tv");
+                startActivity(intent);
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
     }
 
     public void INDEPENDENT_NEWS(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","https://www.independent24.com/");
-        intent.putExtra("name","Independent");
-        startActivity(intent);
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAGs", "The interstitial wasn't loaded yet.");
+        }
+
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                Intent intent = new Intent(TvNews.this, MainActivity.class);
+                intent.putExtra("url","https://www.independent24.com/");
+                intent.putExtra("name","Independent");
+                startActivity(intent);
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
     }
 
     public void JAMUNA_TV(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","https://www.jamuna.tv/");
-        intent.putExtra("name","Jamuna Tv");
-        startActivity(intent);
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAGs", "The interstitial wasn't loaded yet.");
+        }
+
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                Intent intent = new Intent(TvNews.this, MainActivity.class);
+                intent.putExtra("url","https://www.jamuna.tv/");
+                intent.putExtra("name","Jamuna Tv");
+                startActivity(intent);
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
     }
 
     public void MAASRANGA_TV(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","https://maasranga.tv/");
-        intent.putExtra("name","Maasranga");
-        startActivity(intent);
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAGs", "The interstitial wasn't loaded yet.");
+        }
+
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                Intent intent = new Intent(TvNews.this, MainActivity.class);
+                intent.putExtra("url","https://maasranga.tv/");
+                intent.putExtra("name","Maasranga");
+                startActivity(intent);
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
+
     }
 
     public void NTV_NEWS(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","https://www.ntvbd.com/");
-        intent.putExtra("name","ntv");
-        startActivity(intent);
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAGs", "The interstitial wasn't loaded yet.");
+        }
+
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                Intent intent = new Intent(TvNews.this, MainActivity.class);
+                intent.putExtra("url","https://www.ntvbd.com/");
+                intent.putExtra("name","ntv");
+                startActivity(intent);
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
+
     }
 
     public void RTV_NEWS(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","https://www.rtvonline.com/");
-        intent.putExtra("name","rtv");
-        startActivity(intent);
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAGs", "The interstitial wasn't loaded yet.");
+        }
+
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                Intent intent = new Intent(TvNews.this, MainActivity.class);
+                intent.putExtra("url","https://www.rtvonline.com/");
+                intent.putExtra("name","rtv");
+                startActivity(intent);
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
+
     }
 
     public void SOMOY_NEWS(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","http://www.somoynews.tv/");
-        intent.putExtra("name","Somoy Tv");
-        startActivity(intent);
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAGs", "The interstitial wasn't loaded yet.");
+        }
+
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                Intent intent = new Intent(TvNews.this, MainActivity.class);
+                intent.putExtra("url","http://www.somoynews.tv/");
+                intent.putExtra("name","Somoy Tv");
+                startActivity(intent);
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
+
     }
 
     public void DBC_NEWS(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","https://dbcnews.tv/ ");
-        intent.putExtra("name","DBC News");
-        startActivity(intent);
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAGs", "The interstitial wasn't loaded yet.");
+        }
+
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                Intent intent = new Intent(TvNews.this, MainActivity.class);
+                intent.putExtra("url","https://dbcnews.tv/ ");
+                intent.putExtra("name","DBC News");
+                startActivity(intent);
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
+
     }
 
     public void NEWS_24(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","https://www.news24bd.tv/");
-        intent.putExtra("name","News 24");
-        startActivity(intent);
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAGs", "The interstitial wasn't loaded yet.");
+        }
+
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                Intent intent = new Intent(TvNews.this, MainActivity.class);
+                intent.putExtra("url","https://www.news24bd.tv/");
+                intent.putExtra("name","News 24");
+                startActivity(intent);
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
+
     }
 
     public void BOISHAKHI_TV(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","http://boishakhionline.com/");
-        intent.putExtra("name","Boishakhi tv");
-        startActivity(intent);
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAGs", "The interstitial wasn't loaded yet.");
+        }
+
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                Intent intent = new Intent(TvNews.this, MainActivity.class);
+                intent.putExtra("url","http://boishakhionline.com/");
+                intent.putExtra("name","Boishakhi tv");
+                startActivity(intent);
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
     }
 
     public void SA_TV(View view) {
-        Intent intent = new Intent(this, MainActivity.class);
-        intent.putExtra("url","https://www.satv.tv/");
-        intent.putExtra("name","SA tv");
-        startActivity(intent);
+
+        if (mInterstitialAd != null) {
+            mInterstitialAd.show();
+        } else {
+            Log.d("TAGs", "The interstitial wasn't loaded yet.");
+        }
+
+        mInterstitialAd.setAdListener(new AdListener(){
+            @Override
+            public void onAdClosed() {
+                super.onAdClosed();
+                Intent intent = new Intent(TvNews.this, MainActivity.class);
+                intent.putExtra("url","https://www.satv.tv/");
+                intent.putExtra("name","SA tv");
+                startActivity(intent);
+                mInterstitialAd.loadAd(new AdRequest.Builder().build());
+            }
+        });
+
     }
 }
